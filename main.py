e6941590e760ce40070ca9fbb3ca706bdc7cf0cb
Shohrux <shohruxyigitaliev.1@gmail.com>
2025-03-25 19:48:37 +0500

    update

---
+++ main.py
@@ -0,0 +1,35 @@
+from aiomqtt import Client
+import asyncio
+import sys
+from config import MQTT_BROKER, MQTT_PORT, TOPIC_PUB, TOPIC_SUB, MQTT_USERNAME, MQTT_PASSWORD, ssl_context
+from manager import Ports
+
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+serial_port = Ports(port_name='/dev/ttyACM0', baud_rate=9600)
+
+async def main():
+    async with Client(
+        hostname=MQTT_BROKER,
+        port=MQTT_PORT,
+        username=MQTT_USERNAME,
+        password=MQTT_PASSWORD,
+        tls_context=ssl_context,
+    ) as client:
+
+        await client.subscribe(TOPIC_SUB)
+        print(f"Подписан на {TOPIC_SUB}")
+
+        async for msg in client.messages:
+            command = msg.payload.decode()
+            print(f"Получена команда: {command}")
+            serial_response = serial_port.send_command(command)
+            if isinstance(serial_response, list):
+                serial_response = "\n".join(serial_response)
+            await client.publish(TOPIC_PUB, serial_response.encode())
+            print(f"Ответ отправлен: {serial_response}")
+
+if __name__ == "__main__":
+    asyncio.run(main())
+

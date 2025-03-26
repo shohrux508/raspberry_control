e6b0984a916255af9ec6c112ccb84474aa88970b
shohrux508 <yigitalievshohrux@gmail.com>
2025-03-26 20:21:40 +0500

    update

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
-from aiomqtt import Client
 import asyncio
 import sys
-from config import MQTT_BROKER, MQTT_PORT, TOPIC_PUB, TOPIC_SUB, MQTT_USERNAME, MQTT_PASSWORD, ssl_context, COMPORT
-from manager import Ports
+
+from config import COMPORT
+from manager import Ports, manageBroker
 
 if sys.platform == "win32":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 serial_port = Ports(port_name=COMPORT, baud_rate=9600)
 
+
+async def handle_subscription():
+    async for message in manageBroker.subscribe(topic='devices/1/control'):
+        serial_port.send_command(command=message)
+        print("Received:", message)
+
+
 async def main():
-    async with Client(
-        hostname=MQTT_BROKER,
-        port=MQTT_PORT,
-        username=MQTT_USERNAME,
-        password=MQTT_PASSWORD,
-        tls_context=ssl_context,
-    ) as client:
+    asyncio.create_task(handle_subscription())
+    while True:
+        await asyncio.sleep(5)  # задержка между публикациями
+        response = (serial_port.send_command(command='get-status'))
+        await manageBroker.publish(topic='devices/1/response', command=response)
+        print("Published: True")
 
-        await client.subscribe(TOPIC_SUB)
-        print(f"Подписан на {TOPIC_SUB}")
-
-        async for msg in client.messages:
-            command = msg.payload.decode()
-            print(f"Получена команда: {command}")
-            serial_response = serial_port.send_command(command)
-            if isinstance(serial_response, list):
-                serial_response = "\n".join(serial_response)
-            await client.publish(TOPIC_PUB, 'caught'.encode())
-            print(f"Ответ отправлен: {serial_response}")
 
 if __name__ == "__main__":
+    print("Working!")
     asyncio.run(main())
-
--- manager.py
+++ manager.py
@@ -1,6 +1,11 @@
+
+from aiomqtt import Client
 from serial import Serial
 
 import time
+
+from config import MQTT_BROKER, MQTT_PORT, MQTT_USERNAME, MQTT_PASSWORD, ssl_context
+
 
 class Ports:
     def __init__(self, port_name, baud_rate):
@@ -31,5 +36,38 @@ class Ports:
             line = self.ser.readline().decode().strip()
             if line:
                 response_lines.append(line)
-        return response_lines
+        return '\n'.join(response_lines)
+
+
+class manageBroker:
+    @staticmethod
+    async def publish(topic: str, command: str):
+        async with Client(
+                hostname=MQTT_BROKER,
+                port=MQTT_PORT,
+                username=MQTT_USERNAME,
+                password=MQTT_PASSWORD,
+                tls_context=ssl_context,
+        ) as client:
+            await client.publish(topic, str(command))
+            print(f'Отправлено: {command}')
+
+
+    @staticmethod
+    async def subscribe(topic: str):
+        async with Client(
+                hostname=MQTT_BROKER,
+                port=MQTT_PORT,
+                username=MQTT_USERNAME,
+                password=MQTT_PASSWORD,
+                tls_context=ssl_context,
+        ) as client:
+            await client.subscribe(topic)
+            print('Слушаю: ')
+            async for msg in client.messages:
+                yield msg.payload.decode()
+
+
+
+
 
